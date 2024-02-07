e3bdc4769acafd7f2ab6766191f0a657e2e423e2
James Reid <jamesreid3806@gmail.com>
2024-02-07 12:22:59 +0000

    bld? Build

--- dist/package/declarations/dct/BlurridEncoder.d.ts
+++ dist/package/declarations/dct/BlurridEncoder.d.ts
@@ -1,16 +1,29 @@
 export class BlurridEncoder {
     /**
+     * Create encoder instance from downsized image sample.
      *
-     * @param {object} obj
-     * @param {Buffer} obj.buffer
-     * @param {number} obj.width
-     * @param {number} obj.height
+     * @param {object} obj - Object destructured argument.
+     * @param {Buffer} obj.buffer - Buffer of flat, 4-channel rgba pixel data.
+     * @param {number} obj.width - Width of downsized sample image for blur.
+     * @param {number} obj.height - Height of downsized sample image for blur.
      */
     constructor({ buffer, width, height }: {
         buffer: Buffer;
         width: number;
         height: number;
     });
+    /**
+     * Encode downsized sample image into serialized string containing Blurrid
+     * dct coefficients.
+     *
+     * @param {object} obj - Object destructured argument.
+     * @param {number} [obj.length] - Desired length of encoded string.
+     * @param {string} [obj.subsampling] - Chroma subsampling string of the form
+     *      "Y:Cb:Cr" where Y, Cb, Cr are all 3-bit numbers from 0-7 inclusive,
+     *      corresponding to the sampling rates of luma, chroma blue and chroma
+     *      red dct coefficients in the encoded string.
+     * @returns {string}
+     */
     toString({ length, subsampling }?: {
         length?: number | undefined;
         subsampling?: string | undefined;
--- dist/package/index.d.ts
+++ dist/package/index.d.ts
@@ -61,17 +61,30 @@ declare class BlurridDecoder {
 
 declare class BlurridEncoder {
     /**
+     * Create encoder instance from downsized image sample.
      *
-     * @param {object} obj
-     * @param {Buffer} obj.buffer
-     * @param {number} obj.width
-     * @param {number} obj.height
+     * @param {object} obj - Object destructured argument.
+     * @param {Buffer} obj.buffer - Buffer of flat, 4-channel rgba pixel data.
+     * @param {number} obj.width - Width of downsized sample image for blur.
+     * @param {number} obj.height - Height of downsized sample image for blur.
      */
     constructor({ buffer, width, height }: {
         buffer: Buffer;
         width: number;
         height: number;
     });
+    /**
+     * Encode downsized sample image into serialized string containing Blurrid
+     * dct coefficients.
+     *
+     * @param {object} obj - Object destructured argument.
+     * @param {number} [obj.length] - Desired length of encoded string.
+     * @param {string} [obj.subsampling] - Chroma subsampling string of the form
+     *      "Y:Cb:Cr" where Y, Cb, Cr are all 3-bit numbers from 0-7 inclusive,
+     *      corresponding to the sampling rates of luma, chroma blue and chroma
+     *      red dct coefficients in the encoded string.
+     * @returns {string}
+     */
     toString({ length, subsampling }?: {
         length?: number | undefined;
         subsampling?: string | undefined;
@@ -80,11 +93,16 @@ declare class BlurridEncoder {
 }
 
 /**
- * Load image from file, returning buffer of flat, 3-channel rgb pixel data, and
- * metadata pertaining to the image.
+ * Load image from file, returning buffer of flat, 4-channel rgba pixel data,
+ * and metadata pertaining to the image which is required for.
  *
  * @param {string} path - Relative path to image from point of execution.
- * @param {number} samples - Maximum dimension of image in any direction.
+ * @param {number} samples - Maximum dimension of downsized sample image in any
+ *      direction. Note that, due to max uniform limits in webgl fragment
+ *      shaders, sample sizes greater than 16 will prevent a blur from being
+ *      created using the webgl canvas. In this case the web component will
+ *      set the image loading to `eager`, or will use web workers as a fallback
+ *      if available to render the blur.
  * @returns {Promise.<{buffer:Buffer, width:number, height:number}>} Data buffer
  *      and image metadata.
  */
